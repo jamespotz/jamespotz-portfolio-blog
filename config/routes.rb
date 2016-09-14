7d729586f327f45ee8208463f554116c9de84809
James Robert Rooke <thepoltergeist23@gmail.com>
2016-09-14 22:29:48 +0800

    add asci art

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ Rails.application.routes.draw do
   scope :api do
   	resources :posts
   end
-  
-  root to: 'home#index'
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  root to: 'home#index'
 
   get "*path" => "home#index"
 end
