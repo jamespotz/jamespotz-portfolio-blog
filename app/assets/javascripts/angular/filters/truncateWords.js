da8d2c724fc00b89c42a9a5e75b596afb3b187f8
James Robert Rooke <thepoltergeist23@gmail.com>
2016-09-27 21:11:49 +0800

    fix truncateWords regex split

--- app/assets/javascripts/angular/filters/truncateWords.js
+++ app/assets/javascripts/angular/filters/truncateWords.js
@@ -3,7 +3,7 @@ app.filter('truncateWords', function(){
     if (isNaN(words)) return input;
     if (words <= 0) return '';
     if (input){
-      var inputWords = input.split(/\s/);
+      var inputWords = input.split(/\s+/);
       if (inputWords.length > words){
         input = inputWords.slice(0, words).join(' ') + '...';
       }
